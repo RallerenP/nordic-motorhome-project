774fa58ac78bf60a4a35be1ec9f6dd4fa361b90e
Rasmus Povlsen <rasmus.alexander.povlsen@hotmail.dk>
2020-05-15 12:45:17 +0200

    Close #27 and #18

---
+++ src/main/java/com/nordicmotorhome/motorhomerentals/MotorhomeRentalsApplication.java
@@ -0,0 +1,13 @@
+package com.nordicmotorhome.motorhomerentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorhomeRentalsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotorhomeRentalsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nordicmotorhome/motorhomerentals/MotorhomeRentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nordicmotorhome.motorhomerentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorhomeRentalsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
