cb3836a2ddea4e653bff462db86def7fefc4e529
Rasmus Povlsen <rasmus.alexander.povlsen@hotmail.dk>
2020-05-21 23:25:32 +0200

    close #34 homecontroller

--- src/main/java/com/nordicmotorhome/motorhomerentals/view/HomeController.java
+++ src/main/java/com/nordicmotorhome/motorhomerentals/view/HomeController.java
@@ -1,4 +1,20 @@
 package com.nordicmotorhome.motorhomerentals.view;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String index(HttpServletRequest request, Model model) {
+        HttpSession session = request.getSession();
+
+        return "index";
+    }
+
 }
