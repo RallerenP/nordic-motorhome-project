a41f224e269f43e5caad3b8ad02be48f1770af58
Rasmus Povlsen <rasmus.alexander.povlsen@hotmail.dk>
2020-05-22 10:59:55 +0200

    close #64 user session implementation

--- src/main/java/com/nordicmotorhome/motorhomerentals/MotorhomeRentalsApplication.java
+++ src/main/java/com/nordicmotorhome/motorhomerentals/MotorhomeRentalsApplication.java
@@ -1,17 +1,7 @@
 package com.nordicmotorhome.motorhomerentals;
 
-import com.nordicmotorhome.motorhomerentals.data.entity.Customer;
-import com.nordicmotorhome.motorhomerentals.data.entity.Motorhome;
-import com.nordicmotorhome.motorhomerentals.data.entity.MotorhomeModel;
-import com.nordicmotorhome.motorhomerentals.data.entity.Rental;
-import com.nordicmotorhome.motorhomerentals.data.mappers.MotorhomeMapper;
-import com.nordicmotorhome.motorhomerentals.data.mappers.RentalMapper;
-import com.nordicmotorhome.motorhomerentals.domain.exceptions.NoSuchEntityException;
-import com.nordicmotorhome.motorhomerentals.domain.exceptions.services.AuthenticationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class MotorhomeRentalsApplication {
